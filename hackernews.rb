027d47ec17b768f97f637eabf691f39ed6bc155b
MikaAK <mikakalathil@gmail.com>
2014-06-14 00:16:04 -0700

    Add rescue to invalid urls

--- hackernews.rb
+++ hackernews.rb
@@ -1,5 +1,6 @@
-require './comment'
-require './post'
+#!/usr/bin/env ruby
+require_relative 'comment.rb'
+require_relative 'post.rb'
 
 url = ARGV[0]
 
@@ -8,7 +9,11 @@ unless ARGV.length == 1
   puts 'Usage: hackernews [url]'
   exit
 end
-
+begin
 post = Post.new url
 
 post.to_s
+
+rescue StandardError
+  puts "Invalid Url"
+end
